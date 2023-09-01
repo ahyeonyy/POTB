1c18e7e416aa53cf0684bdd7dc2ef9b6614e9d62
이아현 <qlqlql8448@naver.com>
2023-09-01 11:21:58 +0900

    commit test

---
+++ src/main/java/com/example/demo/PassportOntheBlockApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportOntheBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassportOntheBlockApplication.class, args);
+	}
+
+}
